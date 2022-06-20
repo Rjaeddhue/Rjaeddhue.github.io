eab12dbbc2d5d0eab904d203f1e632be255a075e
Radhika Patel <41076175+Rjaeddhue@users.noreply.github.com>
2022-06-20 12:26:04 -0400

    Update app.js

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -1,21 +1,21 @@
-// $(document).ready(function(){
-//     $('#hero-slider').owlCarousel({
-//         loop:true,
-//         margin:0,
-//         nav:true,
-//         items:1,
-//         dots:false,
-//         navText:['PREV', 'NEXT'],
-//         responsive:{
-//             0:{
+$(document).ready(function(){
+   $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+       items:1,
+      dots:false,
+      navText:['PREV', 'NEXT'],
+        responsive:{
+             0:{
                
-//             },
-//             600:{
+            },
+            600:{
                
-//             },
-//             1000:{
+           },
+          1000:{
                
-//             }
-//         }
-//     })
-// });
+            }
+        }
+    })
+ });
